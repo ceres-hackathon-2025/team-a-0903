488323c48468a9d22c04311f3a515290937719c7
得田悟 <toku71093@gmail.com>
2025-09-04 16:30:34 +0900

    動物の種類データベース追加

--- database/seeders/SpeciesSeeder.php
+++ database/seeders/SpeciesSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Species; // Speciesモデルをインポート
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; // DBファサードをインポート
 
 class SpeciesSeeder extends Seeder
 {
@@ -12,6 +14,41 @@ class SpeciesSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Species::create([
+            'id' => 1, 
+            'name' => '犬'
+        ]);
+        Species::create([
+            'id' => 2, 
+            'name' => '猫'
+        ]);
+        Species::create([
+            'id' => 3, 
+            'name' => 'うさぎ'
+        ]);
+        Species::create([
+            'id' => 4, 
+            'name' => 'ハムスター'
+        ]);
+        Species::create([
+            'id' => 5, 
+            'name' => 'フェレット'
+        ]);
+        Species::create([
+            'id' => 6, 
+            'name' => '鳥類'
+        ]);
+        Species::create([
+            'id' => 7, 
+            'name' => '両生類'
+        ]);
+        Species::create([
+            'id' => 8, 
+            'name' => '爬虫類'
+        ]);
+        Species::create([
+            'id' => 9, 
+            'name' => 'その他'
+        ]);
     }
 }
