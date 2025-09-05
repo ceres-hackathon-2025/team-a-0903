823bf5b6931d511f58f55a7b9878976eceff682d
得田悟 <toku71093@gmail.com>
2025-09-05 12:12:50 +0900

    詳細画面の地図表示

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HospitalController; 
+use App\Http\Controllers\MapController;
 
 Route::get('/', [HospitalController::class, 'index'])->name('index');
 Route::get('/search', [HospitalController::class, 'search'])->name('search');
 Route::get('/hospitals/{id}', [HospitalController::class, 'detail'])->name('detail');
+// 例: /map/1 のようなURLでIDが1の店舗の地図を表示する
+Route::get('/map/{shop}', [MapController::class, 'show'])->name('map.show');
